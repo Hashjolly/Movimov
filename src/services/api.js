e6127275cc894954ef82757df3709212193b7653
Hashjolly <149729784+Hashjolly@users.noreply.github.com>
2024-11-22 11:09:20 +0100

    remove comments

--- src/services/api.js
+++ src/services/api.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_TMDB_BASE_URL, // Assurez-vous que votre base URL est correcte
+  baseURL: import.meta.env.VITE_TMDB_BASE_URL,
   params: {
-    api_key: import.meta.env.VITE_TMDB_API_KEY, // Assurez-vous que votre clé API est correctement définie dans votre fichier .env
+    api_key: import.meta.env.VITE_TMDB_API_KEY,
     language: "fr-FR",
   },
 });
@@ -13,7 +13,7 @@ export const searchMovies = (query) => {
     .get("/search/movie", { params: { query } })
     .catch((error) => {
       console.error("Erreur de recherche : ", error);
-      return { data: { results: [] } }; // Retourner un tableau vide en cas d'erreur
+      return { data: { results: [] } }; 
     });
 };
 
